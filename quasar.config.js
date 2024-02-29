2356c28350ca4210ae07d597d7b2f69ded2010f3
HSHadoww <asd520236@gmail.com>
2024-02-29 10:24:07 +0800

    add

--- quasar.config.js
+++ quasar.config.js
@@ -66,7 +66,7 @@ module.exports = configure(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
       ,
-      publicPath: '/quasar-project-foont/'
+      publicPath: './'
       // analyze: true,
       ,
       env: {
